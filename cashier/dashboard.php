b898750c5d3606e6fde787a228683c7fb6b45941
neilarthur <neilarthurpornela@gmail.com>
2022-07-01 23:21:19 +0800

    cashier dashboard update

--- cashier/dashboard.php
+++ cashier/dashboard.php
@@ -150,7 +150,70 @@ include '../php/connection.php';
 
 
             <div class="container-fluid">
-                
+                  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800"></h1>
+                 </div>
+                 <div class="row">
+                    <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-1">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-2">
+                                                Sales</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">PHP 10,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-1">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Customer</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">10</div>
+                                        </div>
+                                        <div class="col-auto">
+                                           <i class='bx bxs-user fa-2x text-gray-300' ></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                 </div>  
+                 <!-- Graph -->
+                 <div class="row">
+                       <div class="col-lg-6">
+                          <div class="card mb-5">
+                                <div class="card-header">
+                                    Default Card Example
+                                </div>
+                                <div class="card-body mb-5">
+                                    This card uses Bootstrap's default styling with no utility classes added. Global
+                                    styles are the only things modifying the look and feel of this default card example.
+                                </div>
+                            </div>
+                     </div>
+                     <div class="col-lg-6">
+                          <div class="card mb-5">
+                                <div class="card-header">
+                                    Default Card Example
+                                </div>
+                                <div class="card-body mb-5">
+                                    This card uses Bootstrap's default styling with no utility classes added. Global
+                                    styles are the only things modifying the look and feel of this default card example.
+                                </div>
+                            </div>
+                     </div>
+                 </div>
+                    </div>
+                 </div>
             </div>   
         </div> 
     </div>
