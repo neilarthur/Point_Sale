2d7d2c75d84cc98c2e5653a55cf9a8c3f30617b4
neilarthur <neilarthurpornela@gmail.com>
2022-07-01 13:38:10 +0800

    cashier dashboard

---
+++ cashier/sales.php
@@ -0,0 +1,224 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["username"])) {
+    header("location: ../index.php");
+
+}
+
+include '../php/connection.php';
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Dashboard</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="../style/dash.css">
+
+    
+    <!-- Font Awesome -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+      rel="stylesheet"
+    />
+    
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      rel="stylesheet"
+    />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous">
+    </script>
+
+    </head>
+    
+    <body>
+   
+    <div class="page">
+
+        <div class="sidebar">
+            <div class="sidebar-header">
+                <div class="sidebar-logo-container">
+                    <div class="logo-container">
+                        <img class="logo-sidebar" src="../image/logo.png" />
+                    </div>
+                    <div class="sys-name-container mt-2">
+                        <p class="sys-name">
+                            POS and <br>
+                            <span class="brand-subname">
+                            Inventory System
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+             <hr style="color:rgb(255, 255, 255);margin-top:10px;">
+            <div class="sidebar-body">
+                <ul class="navigation-list">
+                    <li class="navigation-list-item">
+                        <a class="navigation-link" href="dashboard.php">
+                            <div class="row">
+                                <div class="col-2">
+                                    <i class='bx bxs-home-alt-2'></i>
+                                </div>
+                                <div class="col-9">
+                                    Home
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="navigation-list-item">
+                        <a class="navigation-link" href="sales.php">
+                            <div class="row">
+                                <div class="col-2">
+                                    <i class='bx bx-money' ></i>
+                                </div>
+                                <div class="col-9">
+                                    Sales
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="navigation-list-item">
+                        <a class="navigation-link" href="customer.php">
+                            <div class="row">
+                                <div class="col-2">
+                                    <i class='bx bx-group'></i>
+                                </div>
+                                <div class="col-9">
+                                    Customer
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                   
+                   <li class="navigation-list-item" style="margin-top: 350px;">
+                        <div class="dropdown pb-1">
+                            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                <div class="col-2">
+                                    <img src="../image/user_35px.png" />
+                                </div>
+                                <span class="d-none d-sm-inline mx-1"><?php echo $_SESSION["username"];  ?></span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-white text-small shadow" aria-labelledby="dropdownUser1">
+                                <li><a class="dropdown-item" href="#">Settings</a></li>
+                                <li><a class="dropdown-item" href="#">Profile</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="logout.php">Sign out</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+                <!--End Sidebar -->
+
+            </div>
+
+        </div>
+        <!-- Toggle -->
+        <div class="content">
+            <div class="navigationBar">
+                <button id="sidebarToggle" class="btn sidebarToggle">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+                <!-- Search bar -->
+                <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                    <div class="input-group">
+                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."aria-label="Search" aria-describedby="basic-addon2">
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="button">
+                                <i class="fas fa-search fa-sm"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- Main content -->
+            <div class="col py-3 d-flex justify-content-center overflow-auto">
+                <div class="container-fluid">
+                      <div class="row">
+                        <div class="col d-flex justify-content">
+                            <br>
+                            <div class="w-50">
+                                <h2 class="text-dark text-start ps-3 ">Sales</h2>
+                            </div>
+                            <div class="position-left w-50">
+                                <button type="button" class="btn btn-success px-5 pb-2" data-bs-toggle="modal" data-bs-target="#create" style="margin-left: 63%;"><b><i class='bx bxs-plus-circle'></i> </b> ADD</button>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+                    <!-- Table -->
+                    <div class="row">
+                        <div class="col ">
+                            <div class="card">
+                                <div class="card-body rounded-3 m-4 table-responsive-lg">
+                                    <table class="table table-striped align-middle">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Product Name</th>
+                                                <th scope="col">Generic Name</th>
+                                                <th scope="col">Category</th>
+                                                <th scope="col">Price</th>
+                                                <th scope="col">Quantity</th>
+                                                <th scope="col">Amount</th>
+                                                <th scope="col">Profit</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>Rebisco</td>
+                                                <td>Oreo</td>
+                                                <td>biscuit</td>
+                                                <td>PHP 7.00</td>
+                                                <td>3</td>
+                                                <td> PHP 21.00</td>
+                                                <td>PHP 2.00</td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div> 
+    </div>
+
+        </div> 
+    </div>
+
+
+ 
+<script>
+    let sidebarToggle = document.querySelector(".sidebarToggle");
+    sidebarToggle.addEventListener("click", function(){
+        document.querySelector("body").classList.toggle("active");
+        document.getElementById("sidebarToggle").classList.toggle("active");
+    })
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+</body>
+
+</html>
